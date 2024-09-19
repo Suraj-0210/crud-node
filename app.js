c651bc56019599295710894e77661fd6ef590094
Suryakanta Prusty <kantaprustys@gmail.com>
2024-09-20 00:41:12 +0530

    added the user routes

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/crudDB";
+const PORT = 5000;
 
 const app = express();
 
 mongoose.connect(url, { useNewUrlParser: true });
-
 const con = mongoose.connection;
-
 con.on("open", function () {
   console.log("Database is connected..");
+});
+
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
