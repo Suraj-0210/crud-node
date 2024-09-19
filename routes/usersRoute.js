c0e5e4dd54050c42a386eb84a38731420f90ea29
Suryakanta Prusty <kantaprustys@gmail.com>
2024-09-20 01:13:56 +0530

    added  the userUpdate route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const user = require("../models/user");
 
 router.get("/", async (req, res) => {
   try {
@@ -12,7 +13,7 @@ router.get("/", async (req, res) => {
 });
 router.get("/:id", async (req, res) => {
   try {
-    const users = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id);
     res.json(users);
   } catch (error) {
     console.log(`Errors: ${error}`);
@@ -33,6 +34,29 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.json(error.errmsg);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (req.body.email) {
+      user.email = req.body.email;
+    }
+    if (req.body.name) {
+      user.name = req.body.name;
+    }
+    if (req.body.city) {
+      user.city = req.body.city;
+    }
+    if (req.body.age) {
+      user.age = req.body.age;
+    }
+
+    const data = await user.save();
+    res.json(data);
+  } catch (error) {
+    console.log(`Errors: ${error}`);
   }
 });
 
